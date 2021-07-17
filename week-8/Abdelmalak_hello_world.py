b499b8725f7767c19c4868a2c5dffb8575345fad
soliman Abd <marksol36@gmail.com>
2021-07-16 22:24:54 -0400

    Exercise 8.2 - Anaconda, and running hello world in Python

---
+++ week-8/Abdelmalak_hello_world.py
@@ -0,0 +1,12 @@
+# /*
+# ;============================================
+# ; Title: Exercise 8.2
+# ; Author: Professor Krasso
+# ; Date: 16 July 2021
+# ; Modified By: Soliman Abdelmalak
+# ; Description: Anaconda
+# ;===========================================
+# */
+
+#// print hello world using python
+print('Hello World')
